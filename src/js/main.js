d6d6bfbfa6034c2c0cfffda2a2ddbcb173938535
harshdiggal <harshdiggal@outlook.com>
2023-08-26 15:01:37 +0530

    Added mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const mobileMenuButton = document.querySelector(".mobile-menu");
+	const mobileNav = document.querySelector(".mobile-nav");
+	const closeIcon = document.querySelector(".mobile-nav span i");
+
+	mobileMenuButton.addEventListener("click", () => {
+		mobileNav.classList.toggle("closed-menu");
+		mobileNav.classList.toggle("open-menu");
+	});
+
+	closeIcon.addEventListener("click", () => {
+		mobileNav.classList.remove("open-menu");
+		mobileNav.classList.add("closed-menu");
+	});
+
+	const mobileNavLinks = document.querySelectorAll(".mobile-nav ul li a");
+
+	mobileNavLinks.forEach((link) => {
+		link.addEventListener("click", () => {
+			mobileNav.classList.remove("open-menu");
+			mobileNav.classList.add("closed-menu");
+		});
+	});
+});
+
 const year = document.getElementById("year");
 const thisYear = new Date().getFullYear();
 year.setAttribute("datetime", thisYear);
